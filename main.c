5424c8ce74e1a21245300d3fcacda8131e4e29d1
Jose Torres <jtorresocc@gmail.com>
2022-10-12 16:08:27 -0700

    Designed embedded computer center around ATMega32 microcontroller. Added keypad for input. Use LCD and speaker for output. Added functionality to be able to play musical notes stored in memory.

--- main.c
+++ main.c
@@ -1,9 +1,8 @@
 /*
- * Project4.c
+ * Project3.c
  *
- * Created: 2/28/2020 12:39:43 PM
- * Author : Reniel
-            Joe
+ * Created: 2/13/2020 4:30:33 PM
+ * Author : JOE TORRES
  */ 
 
 #include <avr/io.h>
@@ -11,11 +10,60 @@
 #include "avr.h"
 #include "lcd.h"
 
+#define WHOLE_NOTE		1
+#define HALF_NOTE		0.5
+#define QUARTER_NOTE	0.25
+#define EIGHT_NOTE		0.125
+
+#define NUMNOTES sizeof(song)/sizeof(song[0])
+
+//change port to whatever port the speaker is in
+#define SPK_ON		SET_BIT(PORTA, 0)
+#define SPK_OFF		CLR_BIT(PORTA, 0)
+
+struct note {
+	int frequency;
+	int avr_arg;
+	double duration;
+};
+
 char keypad[16] = {
 	'1', '2', '3', 'A',
 	'4', '5', '6', 'B',
 	'7', '8', '9', 'C',
 	'*', '0', '#', 'D'
+};
+
+int start = 0;
+int key_pressed;
+
+const struct note song[] = {
+	{554, 90, QUARTER_NOTE},
+	{493, 101, QUARTER_NOTE},
+	{440, 113, QUARTER_NOTE},
+	{493, 101, QUARTER_NOTE},
+	{554, 90, QUARTER_NOTE},
+	{554, 90, QUARTER_NOTE},
+	{554, 90, HALF_NOTE},
+	{493, 101, QUARTER_NOTE},
+	{493, 101, QUARTER_NOTE},
+	{493, 101, HALF_NOTE},
+	{554, 90, QUARTER_NOTE},
+	{659, 75, QUARTER_NOTE},
+	{659, 75, HALF_NOTE},
+	{554, 90, QUARTER_NOTE},
+	{493, 101, QUARTER_NOTE},
+	{440, 113, QUARTER_NOTE},
+	{493, 101, QUARTER_NOTE},
+	{554, 90, QUARTER_NOTE},
+	{554, 90, QUARTER_NOTE},
+	{554, 90, QUARTER_NOTE},
+	{554, 90, QUARTER_NOTE},
+	{493, 101, QUARTER_NOTE},
+	{493, 101, QUARTER_NOTE},
+	{554, 90, QUARTER_NOTE},
+	{493, 101, QUARTER_NOTE},
+	{440, 113, WHOLE_NOTE}
 };
 
 int is_pressed(int row, int column) {
@@ -40,6 +88,7 @@ int is_pressed(int row, int column) {
 	return 0;
 }
 
+
 int get_key() {
 	int row, col;
 	for(row = 0; row < 4; ++row) {
@@ -52,116 +101,79 @@ int get_key() {
 	return 0;
 }
 
-unsigned int get_sample() {
-	//configure AVC analog reference
-	//configure ADC mux
-	DDRA = 0x00;
-	CLR_BIT(DDRA, 0);
-	CLR_BIT(PORTA, 0);
-	
-	ADMUX = 0x00;
-	CLR_BIT(ADMUX, REFS1);
-	SET_BIT(ADMUX, REFS0);
-	
-	//enable it
-	//start conversion
-	ADCSRA = 0x00;
-	SET_BIT(ADCSRA, ADEN);
-	SET_BIT(ADCSRA, ADSC);
-	
-	//wait for conversion to complete
-	while(GET_BIT(ADCSRA, ADSC)) {
+void
+avr_wait2(unsigned short msec)
+{
+	TCCR0 = 3;
+	while (msec--) {
+		TCNT0 = (unsigned char)(256 - (XTAL_FRQ / 64) * 0.000001);
+		SET_BIT(TIFR, TOV0);
+		WDR();
+		while (!GET_BIT(TIFR, TOV0));
 	}
+	TCCR0 = 0;
+}
+
+/*
+void playNote(int frequency, int duration) {
+	int k = frequency * duration;
 	
-	return ADC;
+	int period = (1 / frequency);
+	int t_on = period / 2;
+	int t_off = period / 2;
+
+	for(int i = 0; i < k; ++i) {
+		SPK_ON;
+		avr_wait2(101); // change avr_wait implementation?
+		SPK_OFF;
+		avr_wait2(101);
+	}
+}
+*/
+
+void playNote(int frequency, int avr_arg, double duration) {
+	double k = frequency * duration;
+	
+	for(int i = 0; i < k; ++i) {
+		SPK_ON;
+		avr_wait2(avr_arg);
+		SPK_OFF;
+		avr_wait2(avr_arg);
+	}
 }
 
-int get_decimal(float voltage) {
-	return (voltage - (int)voltage) * 100;
-}
-
-void show_empty() {
-	lcd_pos(0, 0);
-	lcd_puts2("IN:");
-	lcd_pos(0, 8);
-	lcd_puts2("MX:--- ");
-	lcd_pos(1, 0);
-	lcd_puts2("MN:--- ");
-	lcd_pos(1, 8);
-	lcd_puts2("AV:--- ");
+void playSong() {
+	for(int i = 0; i < NUMNOTES; ++i) {
+		playNote(song[i].frequency, song[i].avr_arg, song[i].duration);
+		
+		key_pressed = get_key() - 1;
+		if(keypad[key_pressed] == '#') {
+			start = 0;
+			break;
+		}
+	}
 }
 
 int main(void)
 {
+	//initialize speaker port
+	SET_BIT(DDRA, 0); // set DDR to whatever port the speaker is in
 	lcd_init();
-	show_empty();
-	int start = 0;
-	char buf[4];
+	lcd_pos(0, 0);
+	lcd_puts2("SONG: MARY HAD..");
+	lcd_pos(1, 0);
+	lcd_puts2("VOLUME: MIDDLE");
 	
-	float min = 0.0;
-	float max = 0.0;
-	float sum = 0.0;
-	float count = 0.0;
-	
-	int first_read = 1;
-	
-    while (1) 
-    {
-		int key_pressed = get_key() - 1;
+	while (1)
+	{
+		key_pressed = get_key() - 1;
 		
-		//if button is pressed, start collecting voltage
-		if(keypad[key_pressed] == 'A') {
+		if(keypad[key_pressed] == '*') {
 			start = 1;
-			first_read = 1;
-		} else if(keypad[key_pressed] == 'B') {
-			start = 0;
-			min = 0.0;
-			max = 0.0;
-			sum = 0.0;
-			count = 0.0;
-			first_read = 1;
-		}
-
-			//get instantaneous voltage
-			float voltage = (get_sample() / 1023.0) * 5.0;
-			sprintf(buf, "%d.%02d", (int)voltage, get_decimal(voltage));
-			lcd_pos(0, 3);
-			lcd_puts2(buf);
-		if(start) {
-			
-			//get max voltage
-			if(voltage > max) {
-				sprintf(buf, "%d.%02d", (int)voltage, get_decimal(voltage));
-				lcd_pos(0, 11);
-				lcd_puts2(buf);
-				max = voltage;
-			}
-			
-			//get min voltage
-			if(first_read || voltage < min) {
-				if(first_read) {
-					first_read = 0;
-				}
-				
-				sprintf(buf, "%d.%02d", (int)voltage, get_decimal(voltage));
-				lcd_pos(1, 3);
-				lcd_puts2(buf);
-				min = voltage;
-			}
-			
-			//get average voltage
-			sum += voltage;
-			count += 1.0;
-			float avg_voltage = sum / count;
-			
-			sprintf(buf, "%d.%02d", (int)avg_voltage, get_decimal(avg_voltage));
-			lcd_pos(1, 11);
-			lcd_puts2(buf);
-		} else {
-			show_empty();
 		}
 		
-		avr_wait(500);
-    }
+		if(start == 1) {
+			playSong();
+		}
+	}
 }
-
